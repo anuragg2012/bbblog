fcf476f530e09590ce5497da2552f12e0ecad250
anurag2012 <anurag.anurag2012@gmail.com>
2024-03-21 21:22:14 +0530

    routing issue fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { FooterComponent } from './../shared/footer/footer.component';
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NavBarComponent } from '../shared/nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
@@ -11,7 +11,8 @@ import { AboutComponent } from './about/about.component';
   standalone: true,
   imports: [CommonModule, RouterOutlet,RouterLink, RouterLinkActive,NavBarComponent,FooterComponent,HomeComponent,AboutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers:[[{provide: LocationStrategy, useClass: HashLocationStrategy}]]
 })
 export class AppComponent {
   title = 'bb';
